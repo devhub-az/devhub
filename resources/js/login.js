ccade566fb99367a0cd3f1a2833695141977c600
Hose <hose1021@gmail.com>
2020-02-22 21:58:50 +0400

    Removed files

--- resources/js/login.js
+++
@@ -1,5 +0,0 @@
-console.log(
-    '      Using DevHub at work? Work for DevHub.\n')
-
-require('./bootstrap');
-import "@mdi/font/css/materialdesignicons.min.css"
--- resources/js/parser.js
+++
@@ -1,9 +0,0 @@
-import marked from 'marked'
-import emoji from 'node-emoji'
-
-export function parse (markdown) {
-    const replacer = (match) => emoji.emojify(match);
-    markdown = markdown.replace(/(:.*:)/g, replacer);
-
-    return marked(markdown)
-}
