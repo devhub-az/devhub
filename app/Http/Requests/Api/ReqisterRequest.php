bed9ef1661122afc776f20151d8165bf753accab
Michael Hose <hose1021@users.noreply.github.com>
2020-12-14 13:34:32 +0000

    Apply fixes from StyleCI

--- app/Http/Requests/Api/ReqisterRequest.php
+++ app/Http/Requests/Api/ReqisterRequest.php
@@ -5,9 +5,7 @@ namespace App\Http\Requests\Api;
 use Illuminate\Foundation\Http\FormRequest;
 
 /**
- * Class ReqisterRequest
- *
- * @package \App\Http\Requests\Api
+ * Class ReqisterRequest.
  */
 class ReqisterRequest extends FormRequest
 {
