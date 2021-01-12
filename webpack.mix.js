08b857020421a17764d6af9f489f6296ccc82ac1
Hose <hose1021@gmail.com>
2021-01-12 06:09:17 +0400

    Laravel mix upgrade

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,9 @@
 const mix = require('laravel-mix');
+const tailwindcss = require("tailwindcss");
 
 mix.disableNotifications();
 
 mix.copy('node_modules/animate.css/animate.min.css', 'public/css')
-    .vue()
     //Pages
     .js('resources/js/pages/home.js', 'public/js')
     .js('resources/js/pages/hubs.js', 'public/js')
@@ -16,10 +16,9 @@ mix.copy('node_modules/animate.css/animate.min.css', 'public/css')
     .js('resources/js/includes/header.js', 'public/js')
     .js('resources/js/login.js', 'public/js')
     .sass('resources/sass/login.scss', 'public/css')
-    .postCss("resources/sass/tailwind.css", "public/css", [
-        require("tailwindcss"),
-    ])
+    .postCss("resources/sass/tailwind.css", "public/css",[tailwindcss('./tailwind.config.js')])
     .postCss("resources/sass/vendor/balloon.css", "public/css")
+    .vue()
 
 if (mix.inProduction()) {
     mix.version()
