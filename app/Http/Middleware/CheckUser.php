028a7f0f7e8863311f978d452baaa94e23841a19
Hose <hose1021@gmail.com>
2021-01-20 23:44:20 +0400

    add: Middlewares

---
+++ app/Http/Middleware/CheckUser.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Carbon\CarbonImmutable;
+use Closure;
+use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Http\Request;
+
+class CheckUser
+{
+    /**
+     * The authentication factory instance.
+     *
+     * @var \Illuminate\Contracts\Auth\Factory
+     */
+    protected $auth;
+
+    /**
+     * The immutable carbon instance.
+     *
+     * @var \Carbon\CarbonImmutable
+     */
+    protected $carbon;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Factory  $auth
+     * @param  \Carbon\CarbonImmutable             $carbon
+     * @return void
+     */
+    public function __construct(Auth $auth, CarbonImmutable $carbon)
+    {
+        $this->carbon = $carbon;
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($this->auth->check()) {
+            $request->user()->last_active = $this->carbon->now();
+            $request->user()->save();
+        }
+        return $next($request);
+    }
+}
---
+++ app/Http/Middleware/OnlineStatus.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Carbon\Carbon;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+
+class OnlineStatus
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::check()){
+            $expiresAt = Carbon::now()->addMinutes(5);
+            Cache::put('user-online-'.Auth::user()->id,true,$expiresAt);
+        }
+        return $next($request);
+    }
+}
