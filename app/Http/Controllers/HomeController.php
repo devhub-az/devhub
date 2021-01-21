a6558757526ffa52bb414300f28d831149bb22e0
Hose <hose1021@gmail.com>
2021-01-21 21:18:34 +0400

    fix: top hubs on home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,34 +13,54 @@ class HomeController extends Controller
 {
     public function postsApiRoute(Request $request)
     {
-        $lastAuthors = new AuthorsResource(User::orderBy('created_at', 'DESC')->take(5)->get());
-        $top_followed_hubs = new HubsResource(Hub::take(5)->get());
+        $lastAuthors       = new AuthorsResource(User::orderBy('created_at', 'DESC')->take(5)->get());
+        $top_followed_hubs = new HubsResource(
+            Hub::withCount('favorites')->orderBy(
+                'favorites_count',
+                'desc'
+            )->take(5)->get()
+        );
         switch ($request->path()) {
             case '/':
                 session(['main-page' => '/']);
 
                 return view(
                     'pages.home',
-                    ['url' => '/api/articles/filter/day', 'top_followed_hubs' => $top_followed_hubs, 'lastAuthors' => $lastAuthors]
+                    ['url'               => '/api/articles/filter/day',
+                     'top_followed_hubs' => $top_followed_hubs,
+                     'lastAuthors'       => $lastAuthors,
+                    ]
                 );
             case 'top/week':
                 session(['main-page' => '/top/week']);
 
                 return view(
                     'pages.home',
-                    ['url' => '/api/articles/filter/week', 'top_followed_hubs' => $top_followed_hubs, 'lastAuthors' => $lastAuthors]
+                    ['url'               => '/api/articles/filter/week',
+                     'top_followed_hubs' => $top_followed_hubs,
+                     'lastAuthors'       => $lastAuthors,
+                    ]
                 );
             case 'top/month':
                 session(['main-page' => '/top/month']);
 
                 return view(
                     'pages.home',
-                    ['url' => '/api/articles/filter/month', 'top_followed_hubs' => $top_followed_hubs, 'lastAuthors' => $lastAuthors]
+                    ['url'               => '/api/articles/filter/month',
+                     'top_followed_hubs' => $top_followed_hubs,
+                     'lastAuthors'       => $lastAuthors,
+                    ]
                 );
             case 'all':
                 session(['main-page' => '/all']);
 
-                return view('pages.home', ['url' => '/api/articles', 'top_followed_hubs' => $top_followed_hubs, 'lastAuthors' => $lastAuthors]);
+                return view('pages.home',
+                            [
+                                'url'               => '/api/articles',
+                                'top_followed_hubs' => $top_followed_hubs,
+                                'lastAuthors'       => $lastAuthors,
+                            ]
+                );
             case 'favorite' && Auth::user()->followings(Hub::class)->count() !== null
                 && Auth::user()
                     ->followings()
@@ -49,7 +69,10 @@ class HomeController extends Controller
 
                 return view(
                     'pages.home',
-                    ['url' => '/api/articles/filter/favorite', 'top_followed_hubs' => $top_followed_hubs, 'lastAuthors' => $lastAuthors]
+                    ['url'               => '/api/articles/filter/favorite',
+                     'top_followed_hubs' => $top_followed_hubs,
+                     'lastAuthors'       => $lastAuthors,
+                    ]
                 );
             default:
                 abort(404);
