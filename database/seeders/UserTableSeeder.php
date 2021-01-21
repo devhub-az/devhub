0cf915d436c7b0a59fa512027a6c570877a1aa82
Hose <hose1021@gmail.com>
2021-01-21 19:04:19 +0400

    edit: Admin user removed

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -14,22 +14,7 @@ class UserTableSeeder extends Seeder
 {
     public function run()
     {
-        if (App::environment() === 'prod') {
-            User::create(
-                [
-                    'id'           => 'd80644a7-929e-46fa-8a07-6067e3610afa',
-                    'name'         => 'Admin',
-                    'username'     => config('devhub.admin_name'),
-                    'email'        => config('devhub.admin_email'),
-                    'password'     => Hash::make(config('devhub.admin_password')),
-                    'status'       => true,
-                    'is_admin'     => true,
-                    'confirm_code' => Str::random(64),
-                    'created_at'   => Carbon::now(),
-                    'updated_at'   => Carbon::now(),
-                ]
-            );
-        } else {
+        if (App::environment() !== 'prod') {
             User::create(
                 [
                     'id'           => 'd80644a7-929e-46fa-8a07-6067e3610afa',
