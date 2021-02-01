cd1ec75f3b26bc4da1da5ec608eb68e94eff5ecf
Michael Hose <hose1021@users.noreply.github.com>
2021-02-01 05:30:16 +0000

    Apply fixes from StyleCI

--- resources/lang/az/passwords.php
+++ resources/lang/az/passwords.php
@@ -1,9 +1,9 @@
 <?php
 
-return array (
-  'password' => 'Şifrə ən az 6 simvoldan ibarət olmalı və təkrarı ilə uyğun olmalıdır',
-  'reset' => 'Şifrə yeniləndi',
-  'sent' => 'Şifrə yeniləmə adresi sizə email olaraq göndərildi',
-  'token' => 'Bu şifrə yeniləmə kodu yanlışdır',
-  'user' => 'Bu email\'ə uyğun istifadəçi tapılmadı',
-);
+return [
+    'password' => 'Şifrə ən az 6 simvoldan ibarət olmalı və təkrarı ilə uyğun olmalıdır',
+    'reset' => 'Şifrə yeniləndi',
+    'sent' => 'Şifrə yeniləmə adresi sizə email olaraq göndərildi',
+    'token' => 'Bu şifrə yeniləmə kodu yanlışdır',
+    'user' => 'Bu email\'ə uyğun istifadəçi tapılmadı',
+];
--- resources/lang/en/cruds.php
+++ resources/lang/en/cruds.php
@@ -1,75 +1,68 @@
 <?php
 
-return array (
-  'ability' => 
-  array (
-    'fields' => 
-    array (
-      'created_at' => 'Created at',
-      'created_at_helper' => '',
-      'deleted_at' => 'Deleted at',
-      'deleted_at_helper' => '',
-      'id' => 'ID',
-      'id_helper' => '',
-      'name' => 'Name',
-      'name_helper' => '',
-      'updated_at' => 'Updated at',
-      'updated_at_helper' => '',
-    ),
-    'title' => 'Abilities',
-    'title_singular' => 'Ability',
-  ),
-  'role' => 
-  array (
-    'fields' => 
-    array (
-      'abilities' => 'Abilities',
-      'abilities_helper' => '',
-      'created_at' => 'Created at',
-      'created_at_helper' => '',
-      'deleted_at' => 'Deleted at',
-      'deleted_at_helper' => '',
-      'id' => 'ID',
-      'id_helper' => '',
-      'name' => 'Name',
-      'name_helper' => '',
-      'updated_at' => 'Updated at',
-      'updated_at_helper' => '',
-    ),
-    'title' => 'Roles',
-    'title_singular' => 'Role',
-  ),
-  'user' => 
-  array (
-    'fields' => 
-    array (
-      'created_at' => 'Created at',
-      'created_at_helper' => '',
-      'deleted_at' => 'Deleted at',
-      'deleted_at_helper' => '',
-      'email' => 'Email',
-      'email_helper' => '',
-      'email_verified_at' => 'Email verified at',
-      'email_verified_at_helper' => '',
-      'id' => 'ID',
-      'id_helper' => '',
-      'name' => 'Name',
-      'name_helper' => '',
-      'password' => 'Password',
-      'password_helper' => '',
-      'remember_token' => 'Remember Token',
-      'remember_token_helper' => '',
-      'roles' => 'Roles',
-      'roles_helper' => '',
-      'updated_at' => 'Updated at',
-      'updated_at_helper' => '',
-    ),
-    'title' => 'Users',
-    'title_singular' => 'User',
-  ),
-  'userManagement' => 
-  array (
-    'title' => 'User management',
-    'title_singular' => 'User management',
-  ),
-);
+return [
+    'ability' => [
+        'fields' => [
+            'created_at' => 'Created at',
+            'created_at_helper' => '',
+            'deleted_at' => 'Deleted at',
+            'deleted_at_helper' => '',
+            'id' => 'ID',
+            'id_helper' => '',
+            'name' => 'Name',
+            'name_helper' => '',
+            'updated_at' => 'Updated at',
+            'updated_at_helper' => '',
+        ],
+        'title' => 'Abilities',
+        'title_singular' => 'Ability',
+    ],
+    'role' => [
+        'fields' => [
+            'abilities' => 'Abilities',
+            'abilities_helper' => '',
+            'created_at' => 'Created at',
+            'created_at_helper' => '',
+            'deleted_at' => 'Deleted at',
+            'deleted_at_helper' => '',
+            'id' => 'ID',
+            'id_helper' => '',
+            'name' => 'Name',
+            'name_helper' => '',
+            'updated_at' => 'Updated at',
+            'updated_at_helper' => '',
+        ],
+        'title' => 'Roles',
+        'title_singular' => 'Role',
+    ],
+    'user' => [
+        'fields' => [
+            'created_at' => 'Created at',
+            'created_at_helper' => '',
+            'deleted_at' => 'Deleted at',
+            'deleted_at_helper' => '',
+            'email' => 'Email',
+            'email_helper' => '',
+            'email_verified_at' => 'Email verified at',
+            'email_verified_at_helper' => '',
+            'id' => 'ID',
+            'id_helper' => '',
+            'name' => 'Name',
+            'name_helper' => '',
+            'password' => 'Password',
+            'password_helper' => '',
+            'remember_token' => 'Remember Token',
+            'remember_token_helper' => '',
+            'roles' => 'Roles',
+            'roles_helper' => '',
+            'updated_at' => 'Updated at',
+            'updated_at_helper' => '',
+        ],
+        'title' => 'Users',
+        'title_singular' => 'User',
+    ],
+    'userManagement' => [
+        'title' => 'User management',
+        'title_singular' => 'User management',
+    ],
+];
