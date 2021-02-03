37e05695ea1d02ed8aa3449b33bac9c802e5a12b
Hose <hose1021@gmail.com>
2021-02-03 11:06:04 +0400

    Add missed model

---
+++ app/Models/Comment.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use App\Helpers\HasAuthor;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
+class Comment extends Model
+{
+    use HasAuthor;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $table = 'comments';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $casts = [
+        'id' => 'integer'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Relationship: commentable models
+     *
+     * @return MorphTo
+     */
+    public function commentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function articles(): MorphToMany
+    {
+        return $this->morphedByMany(Article::class, 'commentable');
+    }
+}
