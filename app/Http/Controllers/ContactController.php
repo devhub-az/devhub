70e94c069a87e5f18e3de5f0bed3a29bb4eb8797
hose1021 <hose1021@users.noreply.github.com>
2021-04-08 16:08:30 +0000

    Fix styling

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function show(): Factory|View|Application
+    public function show(): Factory | View | Application
     {
         return view('pages.contact');
     }
