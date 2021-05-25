547585ee0a5a927616d190a8103cc8262f3c0a1b
Elkhan <is.elxan@gmail.com>
2021-05-26 00:01:42 +0400

    feature: first steps

---
+++ front/src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        It works!
+      </p>
+    </div>
+  );
+}
+
+export default App;
