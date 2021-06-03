2bcfbda1ce624664b9e0def351feb53bfa9cafa4
Elkhan <is.elxan@gmail.com>
2021-06-04 02:40:03 +0400

    hotfix: reinstall packages

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './app/App';
 import { rootReducer } from './redux/reducer/rootReducer';
